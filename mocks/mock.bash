246ae997ce6523749fa7c2d6f0cf5f2ff259503b
Ícaro Pires <icaropsa@gmail.com>
2020-11-18 15:14:10 -0300

    List real topics for commands receiving mock and handle some errors

--- mocks/mock.bash
+++ mocks/mock.bash
@@ -2,18 +2,20 @@
 
 # WARNING: kills all background jobs *in this shell session* after execution
 
-TOPICS="/topic /rosout"
+TOPICS="/environment/pressure /environment/temperature /image/image_raw /coordinates /leds /commands/leds /commands/camera/rotation /commands/move"
 OUTPUT_DIR="mock_output"
 
 mkdir -p ${OUTPUT_DIR}
 
 echo 'Starting collecting...'
 for t in $TOPICS; do
-    PYTHONUNBUFFERED=yes ros2 topic echo --csv $t > ${OUTPUT_DIR}/log${t//\//_}.csv &
+    PYTHONUNBUFFERED=yes ros2 topic echo --csv $t &> ${OUTPUT_DIR}/log${t//\//_}.csv \
+    || echo "Failed. Does topic $t exists?" \
+    &
 done
 echo 'Started! CTRL-C to stop'
 
 wait
 
-jobs -p | xargs kill -9
+jobs -p | xargs kill -9 &> /dev/null
 echo 'Finished collecting'
