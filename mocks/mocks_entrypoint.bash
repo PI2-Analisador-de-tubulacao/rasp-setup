52094568403c2c8b73f45935163429f764ab2471
Ícaro Pires <icaropsa@gmail.com>
2020-11-26 21:58:34 -0300

    Make subscription topics be created if not exists

--- mocks/mocks_entrypoint.bash
+++ mocks/mocks_entrypoint.bash
@@ -2,17 +2,31 @@
 
 # WARNING: kills all background jobs *in this shell session* after execution
 
-TOPICS="/environment/pressure /environment/temperature /image/image_raw /coordinates /leds /commands/leds /commands/camera/rotation /commands/move"
 OUTPUT_DIR="mock_output"
+
+# Topics and types to publish
+# [/environment/pressure]=sensor_msgs/msg/FluidPressure
+# [/environment/temperature]=sensor_msgs/msg/Temperature
+# [/image/image_raw]=sensor_msgs/msg/Image
+# [/coordinates]=geometry_msgs/msg/Pose2D
+# [/leds]=std_msgs/msg/Float32
+
+declare -A SUBSCRIBE_TOPICS=(
+  [/commands/leds]=std_msgs/msg/Float32
+  [/commands/camera/rotation]=geometry_msgs/msg/Vector3
+  [/commands/camera/height]=std_msgs/msg/Int8
+  [/commands/move]=geometry_msgs/msg/Vector3
+)
+
 
 mkdir -p ${OUTPUT_DIR}
 
-echo 'Starting collecting...'
-for t in $TOPICS; do
-    echo "Running collector for topic $t..."
-    PYTHONUNBUFFERED=yes ros2 topic echo --csv $t &> ${OUTPUT_DIR}/log${t//\//_}.csv \
-    || echo "Failed. Does topic $t exists?" \
-    &
+TOPICS="${!SUBSCRIBE_TOPICS[@]}"
+echo "Collecting from: $TOPICS"
+
+for topic_ in $TOPICS; do
+    type_=${SUBSCRIBE_TOPICS[$topic_]}
+    PYTHONUNBUFFERED=yes ros2 topic echo --csv $topic_ $type_ &> ${OUTPUT_DIR}/log${topic_//\//_}.csv &
 done
 
 source ros2_ws/install/setup.sh
